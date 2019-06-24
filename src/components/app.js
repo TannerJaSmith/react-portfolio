4049da8d119fb80713cdcd6f772f8a4d6dc290a0
tanner <tannerja.smith@outlook.com>
2019-06-23 21:39:44 -0700

    Custom Functions in React and Using Map to Loop Over Data

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio/portfolio-container.js';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Tanner's Portfolio</h1>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        <PortfolioContainer />
       </div>
     );
   }
